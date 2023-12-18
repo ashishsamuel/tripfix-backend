f247134eeeac28bceb9c7838831fe8ba83cefb72
ashish <ashishsamuelthomson@gmail.com>
2023-12-18 17:44:20 +0530

    route path changed

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const connect =async()=>{
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(cookieParser())
-app.use('/api/v1/auth',authRoute)
+app.use('/auth',authRoute)
 app.use('/api/v1/tours',tourRoute)
 app.use('/api/v1/users',userRoute)
 app.use('/api/v1/review',reviewRoute)
@@ -49,5 +49,9 @@ app.use('/api/v1/booking',bookingRoute)
 
 app.listen(port, ()=>{
     connect()
-    console.log(`server listening on port ${port}`);
+    console.log(`tripfix server listening on port ${port}`);
 })
+
+app.get('/', (req, res) => {
+    res.send(`<h1>tripfix server started.. and waiting for client requests!!!</h1>`);
+  });
